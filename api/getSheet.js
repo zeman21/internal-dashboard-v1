6fadfb310da2e6493f572834fbefd0a2929dbd82
Zeman Mustohid <zeman@FTW-3A-222102.local>
2025-11-17 10:34:25 +0700

    Fix: Update serverless function format and routing config for Vercel/Netlify

--- api/getSheet.js
+++ api/getSheet.js
@@ -5,7 +5,7 @@
  * Method: GET
  */
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS Headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
